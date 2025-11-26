98556cfcf21e655a4acaa1e7944d0886964db9a8
Utkarsh Srivastava <utkarsh.work1405@gmail.com>
2025-11-27 03:54:29 +0530

    fixes and changes to portfolio after years :p

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 export default defineConfig({
-   vite: {
-    plugins: [tailwindcss()],
-  },
-   site: 'https://yourdomain.com',
-  integrations: [sitemap()]
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	site: "https://yourdomain.com",
+	integrations: [sitemap()],
 });
